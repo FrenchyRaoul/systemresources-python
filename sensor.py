023d82b54f011070fdbbcfc170888085a549eb73
Nicholai L'Esperance <nicholai.lesperance@gmail.com>
2022-03-28 17:14:07 -0400

    fixing sensors for new board

--- sensor.py
+++ sensor.py
@@ -2,10 +2,25 @@ from dataclasses import dataclass
 import re
 from subprocess import run
 
+DEFAULT_TEMPERATURE_UNIT = 'C'
+
+
+# Package id 0:  +40.0°C  (high = +80.0°C, crit = +100.0°C)
+# Core 0:        +22.0°C  (high = +80.0°C, crit = +100.0°C)
+# Core 4:        +32.0°C  (high = +80.0°C, crit = +100.0°C)
+# temp1:        +27.8°C  (crit = +105.0°C)
+# Sensor 1:      +7.8°C  (low  = -273.1°C, high = +65261.8°C)
 SENSOR_REGEX = re.compile(r'(?P<name>[^:]+):\s+(?P<current>[+-]\d+\.\d+)°(?P<current_unit>[CF])\s+'
-                          r'\(high\s=\s(?P<high>[+-]\d+\.\d+)°(?P<high_unit>[CF]),\s+'
-                          r'crit\s=\s(?P<crit>[+-]\d+\.\d+)°(?P<crit_unit>[CF])',
+                          r'\('
+                          r'(low\s*=\s*(?P<low>[+-]\d+\.\d+)°(?P<low_unit>[CF]),?\s*)?'
+                          r'(high\s*=\s*(?P<high>[+-]\d+\.\d+)°(?P<high_unit>[CF]),?\s*)?'
+                          r'(crit\s*=\s*(?P<crit>[+-]\d+\.\d+)°(?P<crit_unit>[CF]))?'
+                          r'\)',
                           re.VERBOSE | re.I)
+
+# Composite:     +7.8°C  (low  = -273.1°C, high = +84.8°C)
+#                        (crit = +84.8°C)
+COMPOSITE_REGEX = re.compile(r'\(crit\s*=\s*(?P<crit>[+-]\d+\.\d+)°(?P<crit_unit>[CF])\)')
 
 @dataclass
 class Temperature():
@@ -52,17 +67,30 @@ class Temperature():
 class Sensor():
     name: str
     current: Temperature
+    low: Temperature
     high: Temperature
     crit: Temperature
 
     def __repr__(self):
         return f"<Sensor.{self.name}.{self.current}>"
+    
+    def is_low(self): 
+        return current >= low
 
     def is_high(self):
         return current >= high
 
     def is_crit(self):
         return current >= crit
+    
+    def update_crit(self, text):
+        """
+        some composite temperatures have the critical temp on a separate line. if we encounter this, parse and update the object.
+        """
+        match = COMPOSITE_REGEX.match(text)
+        float(match['crit'] or 'inf')
+        match['crit_unit']
+        self.crit = Temperature(float(match['crit'] or 'inf'), match['crit_unit'])
 
     @staticmethod
     def from_text(text):
@@ -74,8 +102,9 @@ class Sensor():
         return Sensor(
             name=data['name'],
             current=Temperature(float(data['current']), data['current_unit']),
-            high=Temperature(float(data['high']), data['high_unit']),
-            crit=Temperature(float(data['crit']), data['crit_unit']),
+            low=Temperature(float(data['low'] or '-inf'), data['low_unit'] or DEFAULT_TEMPERATURE_UNIT),
+            high=Temperature(float(data['high'] or 'inf'), data['high_unit'] or DEFAULT_TEMPERATURE_UNIT),
+            crit=Temperature(float(data['crit'] or 'inf'), data['crit_unit'] or DEFAULT_TEMPERATURE_UNIT),
         )
 
 
@@ -95,29 +124,33 @@ class SensorGroup():
         adapter = adapter.strip()
         sensors = dict()
         for line in lines:
-            sensor = Sensor.from_text(line)
-            if sensor.name in sensors:
-                raise ValueError(f"another bad assumption, multiple sensors named {sensor.name}")
-            sensors[sensor.name] = sensor
+            line = line.strip()
+            if line:
+                try:
+                    sensor = Sensor.from_text(line)
+                    if sensor.name in sensors:
+                        raise ValueError(f"another bad assumption, multiple sensors named {sensor.name}")
+                    sensors[sensor.name] = sensor
+                except ValueError:
+                    sensor.update_crit(line)
 
         return SensorGroup(name, adapter, sensors)
 
 
 @dataclass
 class Sensors():
-    sensor_groups: list
+    sensor_groups: dict
 
     @staticmethod
     def get_sensors():
         proc = run('sensors', capture_output=True, check=True)
         raw_text = proc.stdout.decode().strip()
-        return Sensors(sensor_groups=[SensorGroup.from_text(text) for text in raw_text.split('\n\n')])
+        sensor_groups = [SensorGroup.from_text(text) for text in raw_text.split('\n\n')]
+        return Sensors({group.name: group for group in sensor_groups})
 
     def to_csv(self, stream):
         columns = "chip,adapter,sensor,current,high,crit,unit\n"
         stream.write(columns)
-        for chip in self.sensor_groups:
-            for sensor_name, sensor in chip.sensors.items():
-                stream.write(f"{chip.name},{chip.adapter},{sensor_name},{sensor.current.celcius.value},{sensor.high.celcius.value},{sensor.crit.celcius.value},C\n")
-
-
+        for name, group in self.sensor_groups.items():
+            for sensor_name, sensor in group.sensors.items():
+                stream.write(f"{name},{group.adapter},{sensor_name},{sensor.current.celcius.value},{sensor.high.celcius.value},{sensor.crit.celcius.value},C\n")
